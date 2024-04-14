918eb9afa6ddb2298887c29c8dad022858b3bd5f
Akshay <varma.akshay121@gmail.com>
2024-04-15 00:04:13 +0530

    post widget

--- components/PostWidget.jsx
+++ components/PostWidget.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import React from 'react';
+import moment from 'moment';
+import Link from 'next/link';
 
-const PostWidget = () => {
-  return (
-    <div>PostWidget</div>
-  )
+
+const PostWidget = ({ recentPosts }) => {
+    // const [relatedPosts, setRelatedPosts] = useState([])
+
+    // useEffect(()=>{
+
+    // },[])
+    console.log("recentPosts", recentPosts)
+    return (
+        <div className='bg-dark rounded-lg px-2 py-2 min-h-72'>
+            <div className='text-blue font-extrabold text-center mb-2'>
+                Recent Posts
+            </div>
+            {recentPosts?.map(post =>
+                <div className='text-gray-light font-mono flex py-2 items-center' key={post.createdAt}>
+                    <img className='rounded-full w-10 h-10' src={post?.featuredImage?.url} alt='Featured image'></img>
+                    <Link href={`/post/${post.slug}`} className='pl-2 cursor-pointer'>
+                        <div className='text-sm'>{post.title}</div>
+                        <div className='text-xs'>{moment(post.createdAt).format('MMM DD, YYYY')}</div>
+                    </Link>
+
+                </div>
+            )}
+        </div>
+    )
 }
 
 export default PostWidget
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,9 @@
 import Head from "next/head";
 import { PostCard, PostWidget, Categories } from "../components";
-import { getPosts } from "../services";
+import { getPosts, getRecentPosts } from "../services";
 
 
-export default function Home({posts}) {
+export default function Home({ posts, recentPosts }) {
   return (
     <div className="container mx-auto px-10 mb-8">
       <Head>
@@ -21,7 +21,7 @@ export default function Home({posts}) {
         </div>
         <div className="lg:col-span-4 col-span-1">
           <div className="lg:sticky relative top-8">
-            <PostWidget />
+            <PostWidget recentPosts={recentPosts} />
             <Categories />
           </div>
         </div>
@@ -33,8 +33,9 @@ export default function Home({posts}) {
 
 export async function getStaticProps() {
   const posts = (await getPosts()) || [];
+  const recentPosts = (await getRecentPosts()) || [];
 
   return {
-    props: { posts }
+    props: { posts, recentPosts }
   }
 }
