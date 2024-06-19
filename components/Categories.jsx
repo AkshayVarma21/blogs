51c827363ee942e3a76e04064c286958a10aff03
Akshay <varma.akshay121@gmail.com>
2024-06-19 15:07:33 +0530

    categories page

--- components/Categories.jsx
+++ components/Categories.jsx
@@ -1,21 +1,27 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import Link from 'next/link';
+import { getCategories } from '../services'
 
-const Categories = ({categories}) => {
+const Categories = () => {
+  const [categories, setCategories] = useState([])
+
+  useEffect(() => {
+    getCategories().then((newCategories) => setCategories(newCategories))
+  }, [])
   return (
     <div className='bg-dark rounded-lg px-2 py-2 min-h-72'>
-    <div className='text-blue font-extrabold text-center mb-2'>
-        Categories
-    </div>
-    {categories?.map(category =>
+      <div className='text-blue font-extrabold text-center mb-2'>
+        Recent Posts
+      </div>
+      {categories?.map(category =>
         <div className='text-gray-light font-mono flex py-2 items-center' key={category.id}>
-            <Link href={`/category/${category.slug}`} className='pl-2 cursor-pointer'>
-                <div className='text-sm'>{category.name}</div>
-            </Link>
+          <Link href={`/category/${category.slug}`} className='pl-2 cursor-pointer'>
+            <div className='text-sm'>{category.name}</div>
+          </Link>
 
         </div>
-    )}
-</div>
+      )}
+    </div>
   )
 }
 
